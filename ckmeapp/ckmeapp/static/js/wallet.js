8d5c371b6b4d616d7e215186e5a8935bd08eab3d
jaa2 <43010335+jaa2@users.noreply.github.com>
2022-08-04 19:06:31 +0000

    Add comments to wallet connection script

--- ckmeapp/ckmeapp/static/js/wallet.js
+++ ckmeapp/ckmeapp/static/js/wallet.js
@@ -1,6 +1,6 @@
-let isConnected = false;
-let provider = null;
-let signer = null;
+// global vars 
+let provider = null; // an interface to th blockchain (eg to ask for the current block # or the balance of the address)
+let signer = null; // an interface for signing tx associated with an address
 
 /*
     Ensure that we have a signer to sign transactions with
@@ -8,12 +8,18 @@ let signer = null;
 async function ensureConnected() {
     // Prompt user to connect site to provider
     if (signer === null) {
-        await window.ethereum.enable();
+        // metamask = injected provider, which injects an interface into the window.ethereum object into the website users visit
         provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        // requesting to connect to the users account using a json RPC call
+        await provider.send('eth_requestAccounts', []);
+
+        // getting the signer (eg an eth account) from metamask
         signer = provider.getSigner();
     }
 }
 
+//called when the user clicks the CK Me button
 async function addCk() {
     // Ensure we have a signer (if the user rejects, an Error will be thrown)
     await ensureConnected();
